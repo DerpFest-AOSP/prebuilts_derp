58487adac1c260122289505e39fe237eb9d07e5a
NurKeinNeid <mralexman3000@gmail.com>
2023-10-21 15:10:38 +0200

    Initialize Gramophone from `1.0.0`
    Change-Id: Iaad4cda7407ea12558a2d4a0f4acb4586bed5dff
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := Gramophone
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := prebuilt/gramophone.apk
+
+include $(BUILD_PREBUILT)
